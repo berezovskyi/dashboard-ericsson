062bb29e974ed33f5472ac943df18988d7b9d2b5
Raghu Nayyar <hey@raghunayyar.com>
2018-01-29 21:58:47 +0100

    Use React Router instead of hyperlink to store state in redux.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,13 +10,17 @@
  */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        Home Page Component
-      </h1>
+      <ul>
+        <li><Link to="/">Home Page</Link></li>
+        <li><Link to="/supplychain">Supply Chain</Link></li>
+        <li><Link to="/warehouse">Warehouse</Link></li>
+        <li><Link to="/intellegentagent">Intelligent Agent</Link></li>
+      </ul>
     );
   }
 }
