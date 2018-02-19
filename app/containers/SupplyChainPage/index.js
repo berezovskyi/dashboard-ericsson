e587593ee9165ff8288b9fb341b1de3a6958fe07
Raghu Nayyar <hey@raghunayyar.com>
2018-02-19 11:47:48 +0100

    Adds very basic card layout.

--- app/containers/SupplyChainPage/index.js
+++ app/containers/SupplyChainPage/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Card from '../../ui/Card/Card';
 import styles from './styles.css';
 
-class SupplyChainPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+class SupplyChainPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1 className={styles.h1}>This is the supply chain page.</h1>
+      <div className={styles.fullpage}>
+        <Card title="This is the card title" helpText>
+          <p>All the visualisations go in here.</p>
+        </Card>
+        <Card title="This is the card title" helpText>
+          <p>All the visualisations go in here.</p>
+        </Card>
+      </div>
     );
   }
 }
---
+++ app/ui/Card/Card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Card.css';
+import HelpImage from '../../images/icons/help.svg';
+
+class Card extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    const { children, title } = this.props;
+
+    return (
+      <div className={styles.cardouter}>
+        <div>
+          <h3 className={styles.cardtitle}>{title}</h3>
+        </div>
+        <div className={styles.helperImage}>
+          <img src={HelpImage} height="24" width="24" alt="Help me" />
+        </div>
+        <div>
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
+
+Card.defaultProps = {
+  helpImage: true,
+  helpText: 'This is the default explanation about this card.',
+  title: 'This is the default title',
+};
+
+Card.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string,
+};
+
+export default Card;
