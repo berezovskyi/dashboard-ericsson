efc633f8a156b42949928fbb51bc6c071c1f4124
Raghu Nayyar <hey@raghunayyar.com>
2018-03-19 15:15:12 +0100

    Adds icons for the sidebar.

--- app/ui/Sidebar/Sidebar.js
+++ app/ui/Sidebar/Sidebar.js
@@ -2,6 +2,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import HomeIcon from '../../shared/media/images/icons/home.svg';
+import WarehouseIcon from '../../shared/media/images/icons/warehouse.svg';
+import SupplyChainIcon from '../../shared/media/images/icons/supplychian.svg';
+import IntellegentAgentIcon from '../../shared/media/images/icons/intellegent.svg';
+
 import styles from './Sidebar.css';
 
 class Sidebar extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -11,25 +16,25 @@ class Sidebar extends React.Component { // eslint-disable-line react/prefer-stat
         <ul>
           <li>
             <NavLink exact activeClassName={styles.active} to="/">
-              <img src="http://via.placeholder.com/50x50" alt="" />
+              <HomeIcon width={50} />
               <span>Home Page</span>
             </NavLink>
           </li>
           <li>
             <NavLink activeClassName={styles.active} to="/supplychain">
-              <img src="http://via.placeholder.com/50x50" alt="" />
+              <SupplyChainIcon width={50} />
               <span>Supply Chain Level</span>
             </NavLink>
           </li>
           <li>
             <NavLink activeClassName={styles.active} to="/warehouse">
-              <img src="http://via.placeholder.com/50x50" alt="" />
+              <WarehouseIcon width={50} />
               <span>Warehouse Level</span>
             </NavLink>
           </li>
           <li>
             <NavLink activeClassName={styles.active} to="/intellegentagent">
-              <img src="http://via.placeholder.com/50x50" alt="" />
+              <IntellegentAgentIcon width={50} />
               <span>Intelligent Agent Level</span>
             </NavLink>
           </li>
