92b774c6d403284eb5b70c64359068cb08816806
Raghu Nayyar <hey@raghunayyar.com>
2018-04-04 17:11:50 +0200

    Adds URLs.

--- app/ui/Header/Header.js
+++ app/ui/Header/Header.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.css';
 
-class Header extends Component { // eslint-disable-line react/prefer-stateless-function
+class Header extends Component {
+  // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className={styles.headerouter}>
         <div className={styles.headerinner}>
           <ul className={styles.tabs}>
-            <li className={styles.active}>Daily</li>
-            <li>Weekly</li>
-            <li>Monthly</li>
-            <li>Yearly</li>
+            <li className={styles.active}>
+              <NavLink activeClassName={styles.active} to="/day">
+                Daily
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName={styles.active} to="/week">
+                Weekly
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName={styles.active} to="/month">
+                Monthly
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName={styles.active} to="/year">
+                Yearly
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
