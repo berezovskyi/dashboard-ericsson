a9525a5d09f1b19c0343a6b84e245de3957eb6e5
Raghu Nayyar <hey@raghunayyar.com>
2018-04-04 17:04:05 +0200

    Removes uneeded CSS file.

--- app/app.js
+++ app/app.js
@@ -13,14 +13,8 @@ import App from './pages/App';
 // Load the favicon, the manifest.json file and the .htaccess file
 /* eslint-disable import/no-unresolved, import/extensions */
 import '!file-loader?name=[name].[ext]!./shared/media/images/favicons/favicon.ico';
-
-
 import '!file-loader?name=[name].[ext]!./manifest.json';
 import 'file-loader?name=[name].[ext]!./.htaccess';
-/* eslint-enable import/no-unresolved, import/extensions */
-
-/* TODO: Try if we can pull this out */
-import 'react-vis/dist/style.css';
 
 const history = createHistory();
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
