0f614428c0bd7177a0ba9b38dcbb42b9bf5041ee
Raghu Nayyar <hey@raghunayyar.com>
2018-04-06 14:57:44 +0200

    Initial Commit.

--- app/pages/SupplyChainPage/Capacity/SingleCapacityModal.js
+++ app/pages/SupplyChainPage/Capacity/SingleCapacityModal.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import styles from './Capacity.css';
-import Progress from '../../../ui/Progress/Progress';
+import Knob from '../../../ui/Knob/Knob';
 
-export default function SingleCapacityModal({ capacity, total }) {
-  if (total !== undefined) {
-    return capacity.valueSeq().filter((i, index) => index < total).map(row => (
-      <div key={row.id} className={styles.row}>
-        <h4 className={styles.title}>{row.name}</h4>
-        <div className={styles.fiveSixth}>
-          <Progress value={row.value} className={styles.fourFifth} />
-        </div>
-        <div className={styles.oneSixth}>
-          <span className={styles.text}>{row.value}{'% completed'}</span>
-        </div>
-      </div>
-    ));
-  }
-  return capacity.valueSeq().map(row => (
-    <div key={row.id} className={styles.row}>
-      <h4 className={styles.title}>{row.name}</h4>
-      <div className={styles.fiveSixth}>
-        <Progress value={row.value} className={styles.fourFifth} />
-      </div>
-      <div className={styles.oneSixth}>
-        <span className={styles.text}>{row.value}{'% completed'}</span>
-      </div>
+export default function SingleCapacityModal({ capacity, type }) {
+  return (
+    <div className={styles.row}>
+      {capacity.valueSeq().map(row => {
+        if (row.type === type) {
+          return (
+            <div key={row.id} className={styles.knobsingleouter}>
+              <div className={styles.knobsingleinner}>
+                <Knob
+                  value={row.value}
+                  height={100}
+                  width={100}
+                  bgColor="#E9EFF4"
+                  fgColor="#4D5AFF"
+                  inputColor="#474F58"
+                />
+                <h4 className={styles.title}>{row.name}</h4>
+                <p className={styles.subtitle}>{row.id}</p>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
-  ));
+  );
 }
