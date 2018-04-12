cf702e580b79d64efd953d49915102aab855368f
Raghu Nayyar <hey@raghunayyar.com>
2018-04-12 16:13:35 +0200

    initial commit for making the header work.

--- app/ui/Header/Header.js
+++ app/ui/Header/Header.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import styles from './Header.css';
+import { getCurrentRoute } from '../../utils/utils';
 
-class Header extends Component { // eslint-disable-line react/prefer-stateless-function
+class Header extends Component {
   render() {
+    const { navigation } = this.props;
+    const search = getCurrentRoute(navigation);
     return (
       <div className={styles.headerouter}>
         <div className={styles.headerinner}>
           <ul className={styles.tabs}>
-            <li className={styles.active}>Daily</li>
-            <li>Weekly</li>
-            <li>Monthly</li>
-            <li>Yearly</li>
+            <li className={search.time === 'day' ? styles.active : ''}>
+              Daily
+            </li>
+            <li className={search.time === 'week' ? styles.active : ''}>Weekly</li>
+            <li className={search.time === 'month' ? styles.active : ''}>Monthly</li>
+            <li className={search.time === 'year' ? styles.active : ''}>Yearly</li>
           </ul>
         </div>
       </div>
@@ -18,4 +24,11 @@ class Header extends Component { // eslint-disable-line react/prefer-stateless-f
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  console.log(state);
+  return {
+    navigation: state.get('route'),
+  };
+}
+
+export default connect(mapStateToProps)(Header);
--- app/ui/Sidebar/Sidebar.js
+++ app/ui/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import WarehouseIcon from '../../shared/media/images/icons/warehouse.svg';
 import SupplyChainIcon from '../../shared/media/images/icons/supplychian.svg';
@@ -7,25 +7,25 @@ import IntellegentAgentIcon from '../../shared/media/images/icons/intellegent.sv
 
 import styles from './Sidebar.css';
 
-class Sidebar extends React.Component { // eslint-disable-line react/prefer-stateless-function
+class Sidebar extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className={styles.sidebarpanel}>
         <ul>
           <li>
-            <NavLink activeClassName={styles.active} to="/supplychain">
+            <NavLink activeClassName={styles.active} to="/supplychain?time=day">
               <SupplyChainIcon width={50} />
               <span>Supply Chain Level</span>
             </NavLink>
           </li>
           <li>
-            <NavLink activeClassName={styles.active} to="/warehouse">
+            <NavLink activeClassName={styles.active} to="/warehouse?time=day">
               <WarehouseIcon width={50} />
               <span>Warehouse Level</span>
             </NavLink>
           </li>
           <li>
-            <NavLink activeClassName={styles.active} to="/intellegentagent">
+            <NavLink activeClassName={styles.active} to="/intellegentagent?time=day">
               <IntellegentAgentIcon width={50} />
               <span>Intelligent Agent Level</span>
             </NavLink>
