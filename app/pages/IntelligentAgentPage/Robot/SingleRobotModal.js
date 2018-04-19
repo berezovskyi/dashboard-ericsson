dbeb21917aa8c4c4575a4c504d5f452e4c69a3c6
Raghu Nayyar <hey@raghunayyar.com>
2018-04-19 15:30:27 +0200

    Fixes highlight in robots.

--- app/pages/IntelligentAgentPage/Robot/SingleRobotModal.js
+++ app/pages/IntelligentAgentPage/Robot/SingleRobotModal.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import styles from './Robot.css';
 import Progress from '../../../ui/Progress/Progress';
+import Checkbox from '../../../ui/Form/Checkbox';
 
-export default function SingleRobotModal({ robots, type }) {
-  return (
-    <div className={styles.row}>
-      {robots.valueSeq().map(row => {
-        return (
-          <div key={row.id}>
-            <div>
-              <h4 className={styles.title}>{row.name}</h4>
-              <Progress value={row.value} />
+class SingleRobotModal extends Component {
+  constructor(props) {
+    super(props);
+    this._handleHighlight = this._handleHighlight.bind(this);
+  }
+
+  _handleHighlight(data) {
+    const dispatch = this.props.dispatch;
+    dispatch({
+      type: 'UPDATE_ROBOT_HIGHLIGHT',
+      id: data.id,
+      highlighted: !data.highlighted,
+    });
+  }
+
+  render() {
+    const { robots } = this.props;
+    return robots.valueSeq().map(row => {
+      return (
+        <div key={row.id} className={styles.oneRobot}>
+          <div className={styles.row}>
+            <div className={styles.fiveSixth}>
+              <h4 className={styles.title}>
+                {row.name}{' - '}{row.id}{' - '}{row.to}{' from '}{row.from}
+              </h4>
+            </div>
+            <div className={styles.oneSixth}>
+              <div className={styles.checkboxRobot}>
+                <Checkbox
+                  id={row.id}
+                  checked={row.highlighted}
+                  name="Highlight Entity"
+                  onChange={() => this._handleHighlight(row)}
+                />
+              </div>
             </div>
           </div>
-        );
-      })}
-    </div>
-  );
+          <div className={styles.row}>
+            <div className={styles.fiveSixth}>
+              <Progress value={row.value} />
+            </div>
+            <div className={styles.oneSixth}>
+              <span className={styles['text-modal']}>
+                {row.value}{'% completed'}
+              </span>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
 }
+
+export default connect()(SingleRobotModal);
